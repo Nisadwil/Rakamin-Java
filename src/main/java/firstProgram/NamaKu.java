55f8487ac4c6b1874ed7c613c2769a5cde4f0fae
Annisa Dwi Lestari <45481592+Nisadwil@users.noreply.github.com>
2023-10-22 22:42:17 +0700

    Add files via upload

---
+++ src/main/java/firstProgram/NamaKu.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class NamaKu {
+    public static void main(String[] args) {
+        System.out.println("Name Saya Rakamin");
+    }
+
+    //Fungsi publik (yang bisa dipanggil ditempat lain) bernama Indah
+    public static void Indah(String namaSaya){
+        System.out.println("Name Saya adalah " + namaSaya );
+    }
+}
